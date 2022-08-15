cbc645e646bbb646491163e1af73365ab094c7b9
1-EXON <exon.indiv@gmail.com>
2022-08-15 23:24:32 +0900

    Update: blocks

--- src/index.js
+++ src/index.js
@@ -11,10 +11,12 @@ class Tetris {
         this.map[12][5] = 1
         this.map[12][1] = 1
         
-        setInterval(() => {
-            this.clearMap()
-            this.drawMap()
-        }, 500)
+        this.drawMap()
+
+        // setInterval(() => {
+        //     this.clearMap()
+        //     this.drawMap()
+        // }, 500)
     }
 
     clearMap() {
@@ -23,24 +25,41 @@ class Tetris {
 
     drawMap() {
         let log = ''
-        for (let i = 0; i < 20; i++) {
-            for (let j = 0; j < 10; j++) {
-                if (this.map[i][j] === 1) log += '█'
-                else log += ' '
+        for (let t = 0; t < 7; t++) {
+            console.log('\n')
+            for (let i = 0; i < this.blocks[t].length; i++) {
+                for (let j = 0; j < this.blocks[t][i].length; j++) {
+                    
+                    for (let k = 0; k < this.blocks[t][i][j].length; k++) {
+                        if (this.blocks[t][i][j][k] === 1) log += '█'
+                        else log += ' '
+                    }
+                    log += '\n'
+                }
+                console.log(log)
+                log = ''
             }
-            log += '\n'
         }
+        
+
+        // for (let i = 0; i < 20; i++) {
+        //     for (let j = 0; j < 10; j++) {
+        //         if (this.map[i][j] === 1) log += '█'
+        //         else log += ' '
+        //     }
+        //     log += '\n'
+        // }
         console.log(log)
     }
 
     initBlocks() {
         this.blocks.push([[[1, 1, 1, 1]], [[1], [1], [1], [1]]])
-        this.blocks.push([[[1, 0, 0], [1, 1, 1]], [[0, 1], [0, 1], [1, 1]], [[1, 1, 1], [0, 0, 1]], [[1, 1], [1, 0], [1, 0]]])
-        this.blocks.push([[[0, 0, 1], [1, 1, 1]], [[1, 1], [0, 1], [0, 1]], [[1, 1, 1], [1, 0, 0]], [[1, 0], [1, 0], [1, 1]]])
+        this.blocks.push([[[1, 0, 0], [1, 1, 1], [0, 0, 0]], [[0, 1, 0], [0, 1, 0], [1, 1, 0]], [[0, 0, 0], [1, 1, 1], [0, 0, 1]], [[0, 1, 1], [0, 1, 0], [0, 1, 0]]])
+        this.blocks.push([[[0, 0, 1], [1, 1, 1], [0, 0, 0]], [[1, 1, 0], [0, 1, 0], [0, 1, 0]], [[0, 0, 0], [1, 1, 1], [1, 0, 0]], [[0, 1, 0], [0, 1, 0], [0, 1, 1]]])
         this.blocks.push([[[1, 1], [1, 1]]])
-        this.blocks.push([[[0, 1, 1], [1, 1, 0]], [[1, 0], [1, 1], [0, 1]]])
-        this.blocks.push([[[1, 1, 0], [0, 1, 1]], [[0, 1], [1, 1], [1, 0]]])
-        this.blocks.push([[[0, 1, 0], [1, 1, 1]], [[1, 0], [1, 1], [1, 0]], [[1, 1, 1], [0, 1, 0]], [[0, 1], [1, 1], [0, 1]]])
+        this.blocks.push([[[0, 0, 0], [0, 1, 1], [1, 1, 0]], [[0, 1, 0], [0, 1, 1], [0, 0, 1]], [[0, 1, 1], [1, 1, 0], [0, 0, 0]], [[1, 0, 0], [1, 1, 0], [0, 1, 0]]])
+        this.blocks.push([[[0, 0, 0], [1, 1, 0], [0, 1, 1]], [[0, 0, 1], [0, 1, 1], [0, 1, 0]], [[1, 1, 0], [0, 1, 1], [0, 0, 0]], [[0, 1, 0], [1, 1, 0], [1, 0, 0]]])
+        this.blocks.push([[[0, 1, 0], [1, 1, 1], [0, 0, 0]], [[0, 1, 0], [0, 1, 1], [0, 1, 0]], [[0, 0, 0], [1, 1, 1], [0, 1, 0]], [[0, 1, 0], [1, 1, 0], [0, 1, 0]]])
 
         console.log(JSON.stringify(this.blocks))
     }
